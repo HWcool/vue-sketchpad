4f0100892d044cfd1e4eba7837564c3f6485121a
best-hj <15121152987@163.com>
2021-05-26 12:04:58 +0800

    delete file

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/app.css'
-import './assets/font/iconfont.css'
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
